dc94ae82bb4324700e99bc9d996eb199958a93ad
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-08 08:54:15 -0800

    Add 1-export_to_CSV.py file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Export data in the CSV format. """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    completed = 0
+    url = "https://jsonplaceholder.typicode.com"
+    todos = requests.get(url + "/todos?userId={}".format(sys.argv[1]))
+    user = requests.get(url + "/users/{}".format(sys.argv[1]))
+    username = user.json().get('username')
+
+    with open(f'{sys.argv[1]}.csv', mode='w') as f:
+        for obj in todos.json():
+            completed = obj.get('completed')
+            title = obj.get('title')
+            str1 = "\"{}\",\"{}\",".format(sys.argv[1], username)
+            str2 = "\"{}\",\"{}\"\n".format(completed, title)
+            string = str1 + str2
+            f.write(string)
