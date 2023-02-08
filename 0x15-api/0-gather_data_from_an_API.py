40edcd170eb4d1fe6119ae6cbdd9494d84917ce0
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-08 11:20:34 -0800

    Modify the 1-export_to_CSV.py file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,4 +21,3 @@ if __name__ == "__main__":
     print(emp + "({}/{}):".format(completed, len(todos.json())))
     for title in my_list:
         print('\t', title)
-
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-""" Export data in the CSV format. """
+""" Returns information about his/her TODO list progress. """
+import csv
 import requests
 import sys
-import json
 
 
 if __name__ == "__main__":
     completed = 0
-    url = "https://jsonplaceholder.typicode.com"
-    todos = requests.get(url + "/todos?userId={}".format(sys.argv[1]))
-    user = requests.get(url + "/users/{}".format(sys.argv[1]))
-    username = user.json().get('username')
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    username = requests.get(url + "users/{}".format(user_id)).json()
+    username = username.get('username')
+    user_id = sys.argv[1]
 
-    with open(f'{sys.argv[1]}.json', mode='w') as f:
-        for obj in todos.json():
-            completed = obj.get('completed')
-            title = obj.get('title')
-            str1 = "\"{}\",\"{}\",".format(sys.argv[1], username)
-            str2 = "\"{}\",\"{}\"\n".format(completed, title)
-            string = str1 + str2
-            f.write(string)
+    with open('{}.csv'.format(user_id), 'w', newline="") as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+
+        [write.writerow([user_id, username, i.get("completed"),
+                        i.get("title")]) for i in todos]
