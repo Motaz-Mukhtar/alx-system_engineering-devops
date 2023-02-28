80ee53f43fdaa780ec0eb3b0542aeb47199d106b
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-28 07:56:28 -0800

    HotFixes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'wordpress_fixing':
-  'command': 'sed /s/phpp/php/g /var/www/html/wp-settings.php'
-  'path': '/usr/local/bin/:/bin',
+  'command' => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+  'path'    => '/usr/bin/local/:/bin',
 }
