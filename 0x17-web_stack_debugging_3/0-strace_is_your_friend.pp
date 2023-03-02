4a0ed546e8c2759c4b051f0f7ef558fbfee3c4b5
Motaz-Mukhtar <motazksa2@gmail.com>
2023-03-02 00:59:02 -0800

    Another Hotci

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixing Error that exists in php file
 
 exec { 'wordpress_fixing':
-  'command' => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+  'command' => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   'path'    => '/usr/bin/local/:/bin',
 }
