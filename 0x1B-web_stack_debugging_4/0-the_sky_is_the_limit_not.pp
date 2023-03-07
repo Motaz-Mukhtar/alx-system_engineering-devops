75e7fff9292a7445f3212fbf8d758d74fa7db5c1
Motaz-Mukhtar <motazksa2@gmail.com>
2023-03-07 03:23:03 -0800

    Add web_stack_devugging_4 directory and two files in it

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fixing Error at default nginx_config file
+
+exec {'fix_nginx_config_file':
+  command => 'sed -i s/\'$uri\'/\'http:\/\/localhost\/index.html\'/g /etc/nginx/sites-available/default',
+  path    => 'usr/bin/local/:/bin',
+}
